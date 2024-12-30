bc49d560bc8dd0d6f1300307c46200ac46823971
p-jy <jypark1107@hanmail.net>
2024-12-30 16:13:35 +0900

    자바 01일차: 변수명 작성 규칙 예제

---
+++ java_pjy/src/day01/Ex07_VariableNaming.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Ex07_VariableNaming {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int NUM = 10; //1. 대소문자 구분
+		
+		//int int; //2. 예약어(키워드)는 사용 불가
+		
+		//int 1num = 10; //3. 변수명은 숫자로 시작할 수 없음.
+		
+		int _num = 10; //4. 특수문자는 _와 $만 사용 가능
+		
+		//int n um = 10;
+		//int %num = 10;
+		
+		//int num = 20; //5. 중복 선언 불가
+	}
+
+}
