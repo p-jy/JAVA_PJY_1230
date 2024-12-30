73263dc879ad151edef9ba25bc09c3fea8f40b0b
p-jy <jypark1107@hanmail.net>
2024-12-30 17:30:30 +0900

    자바 01일차 : 산술 연산자 예제

---
+++ java_pjy/src/day01/Ex10_OperatorArithmetic.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Ex10_OperatorArithmetic {
+
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 3;
+		
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+		System.out.println(num1 + " / " + num2 + " = " + (num1 / (double)num2)); // "정수 / 정수" => 정수이므로 "정수 / 실수"로 변환해서 계산
+		System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+		
+	}
+
+}
