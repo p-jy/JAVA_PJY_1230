e1063ae8701b42bcac7d541cbeafc7487c4ae519
p-jy <jypark1107@hanmail.net>
2024-12-30 16:13:19 +0900

    자바 01일차: 변수 오버플로우 예제

---
+++ java_pjy/src/day01/Ex04_VariableOverflow.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Ex04_VariableOverflow {
+
+	public static void main(String[] args) {
+		byte num1 = 127;
+		num1++; //num1에 1을 증가
+		System.out.println(num1);  //byte로 표현 가능한 가장 큰 숫자에 1을 더하면 128이 아닌 -128이 나옴
+		
+		byte num2 = -128;
+		num2--;
+		System.out.println(num2); //byte로 표현 가능한 가장 작은 숫자에 1을 빼면 -129가 아닌 127이 나옴
+		
+	}
+
+}
