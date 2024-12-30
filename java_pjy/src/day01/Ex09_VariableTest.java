d29d54c877b57efc55084079565b09aa3c8dee8f
p-jy <jypark1107@hanmail.net>
2024-12-30 17:30:09 +0900

    자바 01일차: 변수 연습 예제

---
+++ java_pjy/src/day01/Ex09_VariableTest.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex09_VariableTest {
+
+	public static void main(String[] args) {
+		/* 학생의 국어, 영어, 수학 성적을 저장하기 위한 변수를 선언하세요. */
+		int Kor, Eng, Math;
+		
+		/* 학생의 국어, 영어, 수학 성적의 평균을 저장하기 위한 변수를 선언하세요. */
+		double AVG;
+		
+		/* 학생의 학점을 저장하기 위한 변수를 선언하세요. A, B, C, D, F만 저장*/
+		char result1; //문자 하나만 저장하는 것이기 때문에 char를 사용하는 것이 더 적절함
+		String result2;
+	}
+
+}
