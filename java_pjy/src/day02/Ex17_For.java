94573c645c76e1c843883b36bbe4d2c3965e6edf
p-jy <jypark1107@hanmail.net>
2024-12-31 17:06:19 +0900

    자바 02일차 : for 반복문 예제

---
+++ java_pjy/src/day02/Ex17_For.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class Ex17_For {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Hello World!를 5번 출력하는 예제
+		 * - 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * - 규칙성 : Hello World! 출력
+		 * - 반복문 종료 후 : 없음
+		 */
+		
+		int i;
+		for(i = 1; i <= 5; i++) { //초기화 > 조건식 > 실행 > 증감식 > 조건식 > 실행 > 증감식 > 조건식 …
+			System.out.println("Hello World!");
+		}
+
+	}
+
+}
