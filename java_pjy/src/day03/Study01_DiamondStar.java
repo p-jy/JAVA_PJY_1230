c445f4857c274ef277854a140dbf577be3a9646b
p-jy <jypark1107@hanmail.net>
2025-01-02 17:49:32 +0900

    자바 03일차 : 별찍기 연습 (수정 필요)

---
+++ java_pjy/src/day03/Study01_DiamondStar.java
@@ -0,0 +1,45 @@
+package day03;
+
+import java.util.Scanner;
+
+public class Study01_DiamondStar {
+
+	public static void main(String[] args) {
+		/*
+		 *   *
+		 *  ***
+		 * *****
+		 *  ***
+		 *   *
+		 */
+		
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("줄 수를 입력하세요(단, 짝수인 경우 줄-1) : ");
+		 int line = sc.nextInt();
+		 
+		 if(line % 2 == 0) {
+			 line--;
+		 }
+		 
+		 for(int i = 1; i <= line/2+1; i++) {
+			 for(int j = line/2 + 1 - i; j >= 1; j--) {
+				 System.out.print(" ");
+			 }
+			 for(int k = 1; k <= 2*i-1; k++) {
+				 System.out.print("*");
+			 }
+			 System.out.println();
+		 }
+		 for(int i = line/2+2; i >= line/2-1; i--) {
+			 for(int j = 1; j <= line/2-i; j++) {
+				 System.out.print(" ");
+			 }
+			 for(int k = 2*i-line; k >= 1; k--) {
+				 System.out.print("*");
+			 }
+			 System.out.println();
+		 }
+
+	}
+
+}
