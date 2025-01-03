726b5a2a6bdbcd8480c3836d7d3c5294ad0fbaa8
p-jy <jypark1107@hanmail.net>
2025-01-03 09:05:32 +0900

    자바 03일차: 별찍기 연습 (수정2)

--- java_pjy/src/day03/Study01_DiamondStar.java
+++ java_pjy/src/day03/Study01_DiamondStar.java
@@ -31,7 +31,7 @@ public class Study01_DiamondStar {
 			 System.out.println();
 		 }
 		 for(int i = line/2+2; i >= line/2-1; i--) {
-			 for(int j = 1; j <= line/2-i; j++) {
+			 for(int j = 1; j <= line/2-(line/2-1); j++) {
 				 System.out.print(" ");
 			 }
 			 for(int k = 2*i-line; k >= 1; k--) {
