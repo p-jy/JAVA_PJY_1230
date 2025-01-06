e4e300cf162aab0939cabe9dd7b9f8c80a8cd6a3
p-jy <jypark1107@hanmail.net>
2025-01-06 12:47:02 +0900

    자바 04일차 : 과제 최종 수정본

--- java_pjy/src/day04/Ex10_Homework.java
+++ java_pjy/src/day04/Ex10_Homework.java
@@ -40,25 +40,29 @@ public class Ex10_Homework {
 		 */
 		
 
-		char menu = 0;
-		int cnt = 0;
-		int count = 0;
+		char menu;
+
+		int rank = 0;
 		
 		do {
 			printMenu();
 			menu = sc.next().charAt(0);
+			int cnt = 0;
 			
 			switch(menu) {
 			case '1':
-				runGame();
+				cnt = runGame();
+		//		System.out.println(cnt);
+				
+				if(rank > cnt || rank == 0) {
+					rank = cnt;
+				}
 				break;
 			case '2':
-				switch (count) {
-				case 0:
-					System.out.println("게임을 실행한 기록이 없습니다.");
-				default:
-					System.out.println(count + "회");
-				}
+				printRank(rank);
+				System.out.print("메뉴로 가려면 엔터를 입력하세요.");
+				sc.nextLine(); // 메뉴 + 엔터 처리용
+				sc.nextLine(); // 메뉴로 돌아가기 위한 엔터 처리용
 				break;
 			case '3':
 				System.out.println("프로그램을 종료합니다.");
@@ -75,51 +79,58 @@ public class Ex10_Homework {
 	 */
 	
 	public static void printMenu() {
-		System.out.println("메뉴");
+		System.out.println("       메뉴");
+		System.out.println("-----------------");
 		System.out.println("1. UpDown 게임 실행");
 		System.out.println("2. 최고 기록 확인");
 		System.out.println("3. 프로그램 종료");
-		System.out.print("메뉴 선택 : ");
+		System.out.println("-----------------");
+		System.out.print("▶ 메뉴 선택 : ");
 	}
 	
 	/**
 	 * UpDown 게임 메소드
+	 * @return cnt - 최고 기록 확인을 위한 return값
 	 */
 	
-	public static void runGame() {
-		int rdNum = (int)(Math.random() * 100 + 1);
-		int num = 0;
-		do {
-			System.out.print("입력: ");
-			num = sc.nextInt();
-
-			if(num > rdNum) {
-				System.out.println("Down!");
-				cnt++;
-				System.out.println(cnt);
-			} else if(num < rdNum) {
-				System.out.println("Up!");
-				cnt++;
-				System.out.println(cnt);
-			} else {
-				System.out.println("정답!");
-				cnt++;
-				System.out.println(cnt);
-				if(count > cnt || count == 0) {
-					count = cnt;
-				}
-			}
-		} while(num != rdNum);
-	}
-	
-	
-	/**
-	 * 정답 맞추기 횟수를 count하는 메소드
-	 */
-	public static int count() {
-		int count = 0;
+	public static int runGame() {
+		int rdNum = (int)(Math.random() * 100 + 1); // (int)(Math.random() * (max - min + 1) + min)
+		int num;
 		int cnt = 0;
 		
-		return count;
+		do {
+			System.out.print("▶ 입력: ");
+			num = sc.nextInt();
+			cnt++;
+			if(num > rdNum) {
+				System.out.println("Down!");
+	//			System.out.println(cnt);
+			} else if(num < rdNum) {
+				System.out.println("Up!");
+	//			System.out.println(cnt);
+			} else {
+				System.out.println("정답!");
+	//			System.out.println(cnt);
+				
+			}
+		} while(num != rdNum);
+		
+		return cnt;
 	}
+	
+	/**
+	 * 최고 기록 출력 메소드
+	 */
+	
+	public static void printRank(int rank) {
+		if(rank == 0) {
+			System.out.println("게임을 실행한 기록이 없습니다.");
+		} else {
+			
+			System.out.println("[최고 기록]");
+			System.out.println(rank + "회");
+		}
+	//	System.out.println(rank == 0? "게임을 실행한 기록이 없습니다." : rank + "회");
+	}
+	
 }
