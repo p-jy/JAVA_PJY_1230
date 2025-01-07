7f71545ab8ee2539e68afe09b4ceec52537bcc70
p-jy <jypark1107@hanmail.net>
2025-01-07 15:39:01 +0900

    자바 06일차 : 배열 최대값 탐색 예제

---
+++ java_pjy/src/day06/Ex02_ArrayMax.java
@@ -0,0 +1,23 @@
+package day06;
+
+public class Ex02_ArrayMax {
+
+	public static void main(String[] args) {
+		/*
+		 * 배열에 저장된 값 중 가장 큰 값을 출력하는 코드를 작성하세요.
+		 */
+		
+		int [] arr = new int[] {1, 10, 9, 20, 3, 4};
+		
+		int max = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(max <= arr[i]) {
+				max = arr[i];
+			} 
+		}
+		
+		System.out.println("가장 큰 값 : " + max);
+		
+	}
+
+}
