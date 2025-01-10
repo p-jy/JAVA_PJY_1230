f8b771145e1d091bc09f1a5d015cd653941a5a64
p-jy <jypark1107@hanmail.net>
2025-01-10 16:50:17 +0900

    자바 09일차 : 정규표현식 예제

---
+++ java_pjy/src/day09/Ex04_Regex2.java
@@ -0,0 +1,26 @@
+package day09;
+
+import java.util.regex.Pattern;
+
+public class Ex04_Regex2 {
+
+	public static void main(String[] args) {
+		
+		/* 아이디가 주어진 조건에 맞는지 확인하는 코드를 작성.
+		 * 아이디는 숫자, 영문, 특수문자(!@#$)로 이루어져있다.
+		 * 아이디는 최소 8자에서 최대 13자이다.
+		 */
+		
+		String id = "abcd1234$";
+		
+//		String regex = "^[a-zA-Z0-9!@#$]{8,13}$";
+		String regex = "^(\\w|[!@#$]){8,13}$";
+		
+		if(Pattern.matches(regex, id)) {
+			System.out.println(id + "는 사용이 가능합니다.");
+		} else {
+			System.out.println(id + "는 사용이 불가능한 아이디 입니다.");
+		}
+	}
+
+}
