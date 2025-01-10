ab8bbbd8ba8671bde7d6dce8e623f01b6ecbcafd
p-jy <jypark1107@hanmail.net>
2025-01-10 16:52:12 +0900

    자바 09일차 : 기본 클래스 Format

---
+++ java_pjy/src/day09/Ex09_Format.java
@@ -0,0 +1,23 @@
+package day09;
+
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
+
+public class Ex09_Format {
+
+	public static void main(String[] args) {
+		
+		DecimalFormat format = new DecimalFormat("000,000");
+		int num = 12345;
+		System.out.println(format.format(num));
+		
+		String message = "{0} : {1}"; //0 : name, 1 : score
+		String name = "홍길동";
+		int score = 100;
+		//format : 가변 매개변수, 매개변수의 다형성, 박싱, 래퍼클래스
+		String result = MessageFormat.format(message, name, score); 
+		System.out.println(result);
+		
+	}
+
+}
