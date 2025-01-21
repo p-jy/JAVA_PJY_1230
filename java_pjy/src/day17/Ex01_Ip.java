0e520ebadbf378c92e8bf9f2dce84dce6ff7ca81
p-jy <jypark1107@hanmail.net>
2025-01-21 12:49:29 +0900

    자바 17일차 : IP 활용 예제

---
+++ java_pjy/src/day17/Ex01_Ip.java
@@ -0,0 +1,29 @@
+package day17;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Ex01_Ip {
+
+	public static void main(String[] args) {
+		//InetAddress 클래스 예제
+		
+		/* localhost : 내부 아이피 주소에 지정된 이름
+		 * 
+		 */
+		try {
+			InetAddress address = InetAddress.getByName("localhost");
+			System.out.println(address);
+			
+			address = InetAddress.getByName("www.naver.com");
+			System.out.println(address);
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
