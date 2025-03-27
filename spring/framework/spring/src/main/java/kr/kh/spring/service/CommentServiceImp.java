c483c32fd62590d70b722984562278d9ae377a24
p-jy <jypark1107@hanmail.net>
2025-03-27 13:44:20 +0900

    스프링 09일차 : 댓글 작업 중

--- spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
+++ spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
@@ -4,11 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.kh.spring.dao.CommentDAO;
+import kr.kh.spring.model.vo.CommentVO;
 
 @Service
 public class CommentServiceImp implements CommentService {
 
 	@Autowired
 	CommentDAO commentDao;
+	
+	@Override
+	public boolean insertComment(CommentVO comment) {
+		if(comment == null) {
+			return false;
+		}
+		try {
+			return commentDao.insertComment(comment);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 	
 }
