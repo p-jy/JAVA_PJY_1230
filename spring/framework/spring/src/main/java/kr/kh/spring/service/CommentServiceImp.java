60f6abf2293293cb1d45d8973b06aa54a4334ba4
p-jy <jypark1107@hanmail.net>
2025-03-28 09:16:12 +0900

    스프링 09일차 : 댓글 답글 구현

--- spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
+++ spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
@@ -1,28 +1,43 @@
 package kr.kh.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.kh.spring.dao.CommentDAO;
 import kr.kh.spring.model.vo.CommentVO;
+import kr.kh.spring.model.vo.MemberVO;
+import kr.kh.spring.pagination.Criteria;
 
 @Service
 public class CommentServiceImp implements CommentService {
 
 	@Autowired
 	CommentDAO commentDao;
-	
+
 	@Override
-	public boolean insertComment(CommentVO comment) {
+	public boolean insertComment(CommentVO comment, MemberVO user) {
 		if(comment == null) {
 			return false;
 		}
+		if(user == null) {
+			return false;
+		}
 		try {
+			comment.setCo_me_id(user.getMe_id());
 			return commentDao.insertComment(comment);
-		}catch(Exception e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
+
+	@Override
+	public List<CommentVO> getCommentList(Criteria cri) {
+		if(cri == null) {
+			return null;
+		}
+		return commentDao.selectCommentList(cri);
+	}
 }
